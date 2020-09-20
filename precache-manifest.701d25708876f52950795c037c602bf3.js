3a4a4a152793ae0792fea7fecd1ecae3faa7d47e
malikk1510 <69068011+malikk1510@users.noreply.github.com>
2020-09-20 16:01:16 +0530

    Updates

---
+++ precache-manifest.701d25708876f52950795c037c602bf3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8045cc666a3977dd9d6495114ced501f",
+    "url": "/first-webpage/index.html"
+  },
+  {
+    "revision": "895b166d3b01c4f8115b",
+    "url": "/first-webpage/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "46a2c7bfaeef10f71274",
+    "url": "/first-webpage/static/css/main.3ed76a39.chunk.css"
+  },
+  {
+    "revision": "895b166d3b01c4f8115b",
+    "url": "/first-webpage/static/js/2.1198d8a8.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/first-webpage/static/js/2.1198d8a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a2c7bfaeef10f71274",
+    "url": "/first-webpage/static/js/main.fc621aeb.chunk.js"
+  },
+  {
+    "revision": "f242c299f137b15937e3",
+    "url": "/first-webpage/static/js/runtime-main.7902cc64.js"
+  },
+  {
+    "revision": "07f1dcd6cc1cba2e80eda49893d82dbe",
+    "url": "/first-webpage/static/media/cleaning.07f1dcd6.jpg"
+  },
+  {
+    "revision": "1b1a5e9249bc72591a989c9a71daa982",
+    "url": "/first-webpage/static/media/cleaning2.1b1a5e92.jpg"
+  }
+]);
